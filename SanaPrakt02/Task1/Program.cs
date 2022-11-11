d0f43338b1173c0355ceda9ac10f70d0942e6f9f
Vladyslav Geyna <ipz215_gvs@student.ztu.edu.ua>
2022-11-11 22:32:27 +0200

    Text has been changed is last two lines

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -7,8 +7,8 @@
             Console.WriteLine("This project has been created by");
             Console.WriteLine("(C) Vladyslav Geyna");
             Console.WriteLine("2022 - 2023. Sana Commerce Software Development Bootcamp");
-            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+            Console.WriteLine("All rights reserved");
+            Console.WriteLine("Contacts: ipz215_gvs@student.ztu.edu.ua");
         }
     }
 }
