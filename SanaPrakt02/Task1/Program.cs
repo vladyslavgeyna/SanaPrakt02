970bd1d22fdace8a968330c1632ce86d9030df90
Vladyslav Geyna <ipz215_gvs@student.ztu.edu.ua>
2022-11-11 22:29:16 +0200

    Text has beem changed is first three lines

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+            Console.WriteLine("This project has been created by");
+            Console.WriteLine("(C) Vladyslav Geyna");
+            Console.WriteLine("2022 - 2023. Sana Commerce Software Development Bootcamp");
             Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
             Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
         }
