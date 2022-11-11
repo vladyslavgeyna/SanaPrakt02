2d26c836bed92b9f15f3ad81c0a57db6ec6d3061
Vladyslav Geyna <ipz215_gvs@student.ztu.edu.ua>
2022-11-11 22:20:56 +0200

    Console.WriteLine has been duplicated for 5 times

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
         }
     }
 }
